cc740445e516e5560d2df67129d269ee5f680cd7
Anderson Ganhara <aganhara@gmail.com>
2020-01-15 22:16:35 -0300

    Adicionando suporte ao .env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
-
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-pjy6u.gcp.mongodb.net/week10?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_CONNECTION, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
